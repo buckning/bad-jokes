399b8d94200e6489fdefe2181623a43e042db1c8
amcglynn <amcglynn@cisco.com>
2017-10-22 17:14:54 +0100

    Added event to the list of favourites

--- app/src/main/java/andrewmcglynn86/badjokes/FavouritesActivity.kt
+++ app/src/main/java/andrewmcglynn86/badjokes/FavouritesActivity.kt
@@ -2,9 +2,14 @@ package andrewmcglynn86.badjokes
 
 import android.app.ListActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class FavouritesActivity : ListActivity() {
+
+    lateinit var jokes: Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +19,12 @@ class FavouritesActivity : ListActivity() {
     }
 
     fun setDisplayList(values: Array<String>) {
+        jokes = values
         var arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values)
         listAdapter = arrayAdapter
+    }
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+        Toast.makeText(this, jokes.get(position), Toast.LENGTH_SHORT).show()
     }
 }
