41059bc47c5432fe48ad66a3679cc9545ba8b4ac
amcglynn <amcglynn@cisco.com>
2017-10-22 21:45:31 +0100

    Small refactor

--- app/src/main/java/andrewmcglynn86/badjokes/GetLikedJokesTask.kt
+++ app/src/main/java/andrewmcglynn86/badjokes/GetLikedJokesTask.kt
@@ -9,29 +9,10 @@ import android.widget.TextView
  * Created by amcglynn on 19/09/2017.
  */
 class GetLikedJokesTask(var favouritesActivity: FavouritesActivity, var context: Context) : AsyncTask<Void, Void, ArrayList<String>>() {
-    val MAX_LENGTH = 127
 
     override fun doInBackground(vararg params: Void?): ArrayList<String>? {
         var dbHelper = DBHelper(context)
-
-        val db = dbHelper.readableDatabase
-
-        var savedJokes = ArrayList<String>()
-
-        val cursor = db.query(
-                "joke", arrayOf("joke_text", "online_joke_id"), null, null, null, null, null)
-
-        while (cursor.moveToNext()) {
-            var jokeText = cursor.getString(0)
-            jokeText.replace('\n', ' ')
-            if(jokeText.length > MAX_LENGTH) {
-                jokeText = jokeText.substring(0, MAX_LENGTH - 3) + "..."
-            }
-            savedJokes.add(jokeText)
-        }
-        cursor.close()
-
-        return savedJokes
+        return JokeDb().getAllJokes(dbHelper)
     }
 
     override fun onPreExecute() {
--- app/src/main/java/andrewmcglynn86/badjokes/JokeDb.kt
+++ app/src/main/java/andrewmcglynn86/badjokes/JokeDb.kt
@@ -1,16 +1,35 @@
 package andrewmcglynn86.badjokes
 
-/**
- * Created by amcglynn on 22/10/2017.
- */
 class JokeDb {
+    val MAX_JOKE_LENGTH = 127
 
     fun jokeExists(dbHelper: DBHelper, jokeId: String) : Boolean {
         val db = dbHelper.readableDatabase
         val cursor = db.query(
                 "joke", arrayOf("joke_text", "online_joke_id"),
                 "online_joke_id like ?", arrayOf(jokeId), null, null, null);
+        var exists = cursor.moveToFirst()
+        cursor.close()
+        return exists
+    }
 
-        return cursor.moveToFirst()
+    fun getAllJokes(dbHelper: DBHelper) : ArrayList<String> {
+        val db = dbHelper.readableDatabase
+        val savedJokes = ArrayList<String>()
+
+        val cursor = db.query(
+                "joke", arrayOf("joke_text", "online_joke_id"), null, null, null, null, null)
+
+        while (cursor.moveToNext()) {
+            var jokeText = cursor.getString(0)
+            jokeText.replace('\n', ' ')
+            if(jokeText.length > MAX_JOKE_LENGTH) {
+                jokeText = jokeText.substring(0, MAX_JOKE_LENGTH - 3) + "..."
+            }
+            savedJokes.add(jokeText)
+        }
+        cursor.close()
+
+        return savedJokes
     }
 }
